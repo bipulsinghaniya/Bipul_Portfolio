d854a3690872fd41a3e9f6f920cf513175bf528e
Bipul Singhaniya <singhaniyabipul9@gmail.com>
2026-03-10 20:31:10 +0530

    i have added new achievement

--- bipul-portfolio/src/components/Achievements.jsx
+++ bipul-portfolio/src/components/Achievements.jsx
@@ -3,6 +3,22 @@ export default function Achievements() {
     <section id="achievements" className="mt-20">
       <h2 className="text-3xl font-bold mb-6">Achievements</h2>
 
+
+      <div className="bg-white/5 border border-white/10 rounded-2xl p-6 backdrop-blur mt-4">
+  <p className="text-lg font-semibold">
+    🏆 Winner – Defend The Kingdom Hackathon
+  </p>
+
+  <p className="text-gray-400 mt-2">
+    Secured <span className="font-medium text-white">1st place</span> in the
+    “Defend The Kingdom” Hackathon organized by 
+    <span className="text-white"> Infineon Technologies</span>, developing an 
+    innovative solution to tackle real-world engineering challenges and 
+    demonstrating strong problem-solving and teamwork skills.
+  </p>
+</div>
+
+      <br></br>
       <div className="bg-white/5 border border-white/10 rounded-2xl p-6 backdrop-blur">
         <p className="text-lg font-semibold">
           🏆 Top 5 in Graphs Coding Contest
@@ -13,6 +29,9 @@ export default function Achievements() {
           showcasing strong problem-solving and graph algorithm skills.
         </p>
       </div>
+
+
+
     </section>
   );
 }
